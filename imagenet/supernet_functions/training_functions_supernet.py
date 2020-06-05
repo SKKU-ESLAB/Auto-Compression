1fc9090b00d66ee88585570fd2828cd9c7097b74
Hoseung Kim <ghyutjik123@gmail.com>
2020-06-06 03:59:12 +0900

    loading pretrained model

--- imagenet/supernet_functions/training_functions_supernet.py
+++ imagenet/supernet_functions/training_functions_supernet.py
@@ -41,7 +41,7 @@ class TrainerSupernet:
             
             self.logger.info("Firstly, start to train weights for epoch %d" % (epoch))
             #self._training_step(model, train_w_loader, self.w_optimizer, epoch, info_for_logger="_w_step_")
-            self.w_scheduler.step()
+            #self.w_scheduler.step()
 
             top1_avg = self._validate(model, test_loader, epoch)
             if best_top1 < top1_avg:
