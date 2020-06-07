916aaf77a9d3d31a96ad52b4206a0a3328a8297a
Hoseung Kim <ghyutjik123@gmail.com>
2020-06-07 22:36:54 +0900

    Train mode with lr_decay 0.2

--- imagenet/supernet_functions/training_functions_supernet.py
+++ imagenet/supernet_functions/training_functions_supernet.py
@@ -38,6 +38,9 @@ class TrainerSupernet:
         # firstly, train weights only
         for epoch in range(self.train_thetas_from_the_epoch):
             self.writer.add_scalar('learning_rate/weights', self.w_optimizer.param_groups[0]['lr'], epoch)
+
+            if epoch == 5:
+                self.optimizer.param_groups[0]['lr'] *= 0.2
             
             self.logger.info("Firstly, start to train weights for epoch %d" % (epoch))
             self._training_step(model, train_w_loader, self.w_optimizer, epoch, info_for_logger="_w_step_")
