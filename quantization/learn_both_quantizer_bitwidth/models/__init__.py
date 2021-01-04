d3df69bcdd0a279c4327d77c410e9b89b0521f6b
GH-Jo <lkj004124@gmail.com>
2021-01-05 01:45:17 +0900

    Fix model import line

--- quantization/learn_both_quantizer_bitwidth/models/__init__.py
+++ quantization/learn_both_quantizer_bitwidth/models/__init__.py
@@ -2,4 +2,4 @@ from .alexnet import alexnet
 from .alexnet_cifar import alexnet_cifar
 from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
 from .resnet_cifar import resnet20, resnet32, resnet44, resnet56, resnet110, resnet1202
-from .mobilenet_cifar import mobilenetv2, mobilenetv2_cifar
+from .mobilenetv2 import mobilenetv2, mobilenetv2_cifar
