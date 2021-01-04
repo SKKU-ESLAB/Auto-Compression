ab288f285de18e8840b56f46241c13cb67265c73
GH-Jo <lkj004124@gmail.com>
2021-01-04 10:41:37 +0900

    Fix imagenet dataloader

--- quantization/learn_both_quantizer_bitwidth/utils/data_loader.py
+++ quantization/learn_both_quantizer_bitwidth/utils/data_loader.py
@@ -34,15 +34,19 @@ def data_loader(dir, dataset, batch_size, workers):
     elif dataset == 'imagenet':
         traindir = os.path.join(dir, 'train')
         valdir = os.path.join(dir, 'val')
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], 
+                                         std=[0.229, 0.224, 0.225])
         train_transform = transforms.Compose([
             transforms.RandomResizedCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            normalize,
         ])
         val_transform = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            normalize,
         ])
         train_set = datasets.ImageFolder(traindir, transform=train_transform)
         val_set = datasets.ImageFolder(valdir, transform=val_transform)
