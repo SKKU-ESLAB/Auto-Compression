567b18e505b943a1ad20369028f552f1857228ca
GH-Jo <lkj004124@gmail.com>
2021-02-08 04:23:00 +0900

    Create lsq_grad branch

--- quantization/learn_both_quantizer_bitwidth/utils/utils.py
+++ quantization/learn_both_quantizer_bitwidth/utils/utils.py
@@ -171,11 +171,11 @@ def count_memory(model):
 
 def create_exp_dir(path, scripts_to_save=None):
   if not os.path.exists(path):
-    os.mkdir(path)
+    os.makedirs(path, exist_ok=True)
   print('Experiment dir : {}'.format(path))
 
   if scripts_to_save is not None:
-    os.mkdir(os.path.join(path, 'scripts'))
+    os.makedirs(os.path.join(path, 'scripts'))
     for script in scripts_to_save:
       dst_file = os.path.join(path, 'scripts', os.path.basename(script))
       shutil.copyfile(script, dst_file)
