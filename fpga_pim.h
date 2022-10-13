348de5013fc0717e8470dda68d92fce48984edc4
7bvcxz <7bvcxz@gmail.com>
2022-10-13 20:25:59 +0900

    I'm really Ready

---
+++ fpga_pim.h
@@ -0,0 +1,18 @@
+#include <fcntl.h>
+#include <getopt.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <stdbool.h>
+
+void pimExecution(uint8_t *pim_addr, uint8_t *data, bool is_write);
