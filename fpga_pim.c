562873d4b27afc327f148632a9f6b5091cbfb54e
7bvcxz@gmail.com <7bvcxz@gmail.com>
2022-10-27 15:36:18 +0900

    FPGA API Problem Solved

--- fpga_pim.c
+++ fpga_pim.c
@@ -1,6 +1,6 @@
 #include "fpga_pim.h"
 
-uint64_t pimExecution(uint32_t addr, void *data, int iswrite) {
+uint64_t pimExecution(uint32_t addr, uint32_t *data, int iswrite) {
 	printf("   fpga: %x\n", addr);
 	return 1;
 }
