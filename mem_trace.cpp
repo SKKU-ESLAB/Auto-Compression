bab9001652dc4636aa9b9c5516017cc6e008e045
7bvcxz@gmail.com <7bvcxz@gmail.com>
2022-11-02 23:28:57 +0900

    Mem Trace Upgrade..

--- mem_trace.cpp
+++ mem_trace.cpp
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/mman.h>
+#include <chrono>
 
 #define LEN_PIM 0x100000000
 
@@ -18,12 +19,14 @@ uint64_t pim_base;
 uint32_t burstSize = 32;
 
 void set_trace_file(char **argv, char option) {
+	std::cout << " > set trace file\n";
 	fm.open("./mem_trace/"+std::string(argv[1])+option+".txt");
 }
 
 void set_pim_device() {
+	std::cout << " > set pim device\n";
 	fd = open("/dev/PIM", O_RDWR|O_SYNC);
-	return;
+	
 	if (fd < 0)
 		std::cout << "Open /dev/PIM failed...\n";
 	else
@@ -34,6 +37,15 @@ void set_pim_device() {
 }
 
 void trace_and_send() {
+	std::cout << " > trace and send\n";
+
+	typedef std::chrono::high_resolution_clock Time;
+	typedef std::chrono::milliseconds ms;
+	typedef std::chrono::duration<float> fsec;
+
+	buffer = (uint8_t*)calloc(64, sizeof(uint8_t));
+	
+	auto start = Time::now();
 	while(std::getline(fm, line)) {
 		std::stringstream linestream(line);
 		int is_write;
@@ -46,18 +58,23 @@ void trace_and_send() {
 		} else if (is_write == 1) {  // write
 			std::memcpy(pim_mem + hex_addr, buffer, burstSize);
 		} else if (is_write == 2) {  // preprocess end
+			start = Time::now();
 			system("sudo m5 dumpstats");
 		} else {
 			std::cout << "This should not be done... Somethings wrong\n";
 		}
 	}
+	auto end = Time::now();
+	std::cout << "All trace ended\n";
+	fsec time = end - start;
+	std::cout << time.count() << "s\n";
 }
 
 int main(int argc, char **argv) {
 	char option;
 	std::cout << "option : 1 / 2 / 3\nenter option :";
 	std::cin >> option;
-	
+
 	if (argc <= 1) {
 		std::cout << "add, mul, mac, bn, gemv, lstm\n";
 		return -1;
