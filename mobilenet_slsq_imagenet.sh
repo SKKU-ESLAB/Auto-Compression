c84026926bbe367e273d81a6591c0737edb0e855
Podossiu <ilena7440@naver.com>
2022-11-10 11:14:57 +0000

    percentile

--- mobilenet_slsq_imagenet.sh
+++ mobilenet_slsq_imagenet.sh
@@ -25,4 +25,9 @@
 #python3 main.py examples/lsq/mobilenet/imagenet/soft_pruning/mobilenetv2_a8w8_imagenet_7_epoch70.yaml
 #python3 main.py examples/lsq/mobilenet/imagenet/soft_pruning/mobilenetv2_a8w8_imagenet_8_epoch70.yaml
 #python3 main.py examples/lsq/mobilenet/imagenet/soft_pruning/mobilenetv2_a8w8_imagenet_5_epoch70.yaml
-python3 main.py examples/lsq/mobilenet/imagenet/soft_pruning/mobilenetv2_a8w8_imagenet_5_epoch70_percentile0_4.yaml
+#python3 main.py examples/lsq/mobilenet/imagenet/soft_pruning/mobilenetv2_a8w8_imagenet_5_epoch70_percentile0_4.yaml
+#python3 main.py examples/lsq/mobilenet/imagenet/soft_pruning/mobilenetv2_a8w8_imagenet_4_epoch70_percentile0_4.yaml
+#python3 main.py examples/lsq/mobilenet/imagenet/soft_pruning/mobilenetv2_a8w8_imagenet_3_epoch70_percentile0_4.yaml
+#python3 main.py examples/lsq/mobilenet/imagenet/soft_pruning/mobilenetv2_a8w8_imagenet_6_epoch70_percentile0_4.yaml
+#python3 main.py examples/lsq/mobilenet/imagenet/soft_pruning/mobilenetv2_a8w8_imagenet_7_epoch70_percentile0_4.yaml
+python3 main.py examples/lsq/mobilenet/imagenet/soft_pruning/mobilenetv2_a8w8_imagenet_8_epoch70_percentile0_4.yaml
