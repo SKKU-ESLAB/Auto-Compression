0e4c3fecc16f7862d66ed3d09f5fd3b9ebf62bb5
Hayun Lee <lhy920806@gmail.com>
2023-04-30 14:15:17 +0900

    [UVP] Remove original epochs artument

--- pruning/admm_unaligned/main.py
+++ pruning/admm_unaligned/main.py
@@ -37,8 +37,6 @@ parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
                         ' (default: resnet18)')
 parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=90, type=int, metavar='N',
-                    help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
 parser.add_argument('-b', '--batch-size', default=256, type=int,
