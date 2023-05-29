6b322dcfc705d3efbdc8dc45d581a6fb65557254
Hayun Lee <lhy920806@gmail.com>
2023-05-29 13:46:01 +0900

    [UVP] Add get_model_from_gluoncv.py

--- pruning/admm_unaligned/get_model_from_gluoncv.py
+++ pruning/admm_unaligned/get_model_from_gluoncv.py
@@ -7,3 +7,16 @@ import numpy as np
 
 from models.mobilenet_v1 import MobileNetV1
 #from models.mobilenet_v2 import MobileNetV2
+
+def get_pretrained_model(arch, width_mult):
+    # you may modify it to switch to another model. The name is case-insensitive
+    if arch == "mobilenet_v1":
+        model_name = "mobilenet" + str(width_mult)
+        torch_model = MobileNetV1(num_classes=1000, input_size=224, width_mult=width_mult)
+    elif arch == "mobilenet_v2":
+        model_name = "mobilenetv2_" + str(width_mult)
+        #torch_model = MobileNetV2(num_classes=1000, input_size=224, width_mult=width_mult)
+        torch_model = models.mobilenet_v2(width_mult=width_mult)
+    # download and load the pre-trained model
+    mxnet_model = gluoncv.model_zoo.get_model(model_name, pretrained=True)
+    #print(mxnet_model)
