392478d85f42368fd71a5873a18730d1d439eb6c
Hayun Lee <lhy920806@gmail.com>
2023-05-29 12:45:20 +0900

    [UVP] Add get_model_from_gluoncv.py

--- pruning/admm_unaligned/get_model_from_gluoncv.py
+++ pruning/admm_unaligned/get_model_from_gluoncv.py
@@ -4,3 +4,6 @@ import gluoncv
 import torch.nn as nn
 import torchvision.models as models
 import numpy as np
+
+from models.mobilenet_v1 import MobileNetV1
+#from models.mobilenet_v2 import MobileNetV2
