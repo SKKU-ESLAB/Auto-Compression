1a051cfba1dc823c49d076c2c93893310611ccf3
Hayun Lee <lhy920806@gmail.com>
2023-06-05 15:55:47 +0900

    [UVP] working on timm version

--- pruning/UVP/ADMM_timm/train.py
+++ pruning/UVP/ADMM_timm/train.py
@@ -374,3 +374,23 @@ group.add_argument('--repeat', action='store_true', default=False)
 group.add_argument('--rho', default=0.001, type=float)
 parser.add_argument('--cp-alpha', default=0.1, type=float)
 parser.add_argument('--cp-beta', default=0.8, type=float)
+
+
+
+def _parse_args():
+    # Do we have a config file to parse?
+    args_config, remaining = config_parser.parse_known_args()
+    if args_config.config:
+        with open(args_config.config, 'r') as f:
+            cfg = yaml.safe_load(f)
+            parser.set_defaults(**cfg)
+
+    # The main arg parser parses the rest of the args, the usual
+    # defaults will have been overridden if config file specified.
+    args = parser.parse_args(remaining)
+
+    # Cache the args as a text string to save them in the output dir later
+    args_text = yaml.safe_dump(args.__dict__, default_flow_style=False)
+    return args, args_text
+
+
