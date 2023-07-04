c47f887eff925b2d7f6f3d2c8a1f64bc989e0994
Hayun Lee <lhy920806@gmail.com>
2023-07-04 19:41:40 +0900

    [UVP] Pruning code for Transformer

--- pruning/UVP/Transformer/text_classification.py
+++ pruning/UVP/Transformer/text_classification.py
@@ -238,3 +238,60 @@ class DataTrainingArguments:
             )
 
 
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from
+    """
+
+    model_name_or_path: str = field(
+        metadata={
+            "help": (
+                "Path to pretrained model, sparsezoo stub. or model identifier from "
+                "huggingface.co/models"
+            )
+        }
+    )
+    config_name: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "Pretrained config name or path if not the same as model_name"
+        },
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "Pretrained tokenizer name or path if not the same as model_name"
+        },
+    )
+    cache_dir: Optional[str] = field(
+        default=None,
+        metadata={"help": "Where to store the pretrained data from huggingface.co"},
+    )
+    use_fast_tokenizer: bool = field(
+        default=True,
+        metadata={"help": "Whether to use one of the fast tokenizers. Default True"},
+    )
+    use_teacher_tokenizer: bool = field(
+        default=False,
+        metadata={
+            "help": "Whether to use separate tokenizer for distillation teacher. "
+            "Default False; uses same tokenizer for teacher and student"
+        },
+    )
+    model_revision: str = field(
+        default="main",
+        metadata={
+            "help": "The specific model version to use "
+            "(can be a branch name, tag name or commit id)"
+        },
+    )
+    use_auth_token: bool = field(
+        default=False,
+        metadata={
+            "help": "Will use token generated when running `transformers-cli login` "
+            "(necessary to use this script with private models)"
+        },
+    )
+
+
