0ca76910359729ca8a1954cbe1c17e5748ded339
Hayun Lee <lhy920806@gmail.com>
2023-07-04 17:41:29 +0900

    [UVP] Pruning code for Transformer

--- pruning/UVP/Transformer/text_classification.py
+++ pruning/UVP/Transformer/text_classification.py
@@ -87,3 +87,154 @@ metadata_args = [
 ]
 
 
+@dataclass
+class DataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for
+    training and eval
+
+    Using `HfArgumentParser` we can turn this class into argparse
+    arguments to be able to specify them on the command line
+    """
+
+    task_name: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "The name of the task to train on: "
+            + ", ".join(_TASK_TO_KEYS.keys())
+        },
+    )
+    dataset_name: Optional[str] = field(
+        default=None,
+        metadata={"help": "The name of the dataset to use (via the datasets library)"},
+    )
+    dataset_config_name: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": ("The configuration name of the dataset to use"),
+        },
+    )
+    max_seq_length: int = field(
+        default=384,
+        metadata={
+            "help": "The maximum total input sequence length after tokenization. "
+            "Sequences longer  than this will be truncated, sequences shorter will "
+            "be padded."
+        },
+    )
+    overwrite_cache: bool = field(
+        default=False,
+        metadata={"help": "Overwrite the cached preprocessed datasets or not."},
+    )
+    preprocessing_num_workers: Optional[int] = field(
+        default=None,
+        metadata={"help": "The number of processes to use for the preprocessing."},
+    )
+    pad_to_max_length: bool = field(
+        default=True,
+        metadata={
+            "help": "Whether to pad all samples to `max_seq_length`. If False, "
+            "will pad the samples dynamically when batching to the maximum length "
+            "in the batch (which can be faster on GPU but will be slower on TPU)."
+        },
+    )
+    max_train_samples: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": "For debugging purposes or quicker training, truncate the number "
+            "of training examples to this value if set."
+        },
+    )
+    max_eval_samples: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": "For debugging purposes or quicker training, truncate the number "
+            "of evaluation examples to this value if set."
+        },
+    )
+    max_predict_samples: Optional[int] = field(
+        default=None,
+        metadata={
+            "help": (
+                "For debugging purposes or quicker training, truncate the number of "
+                "prediction examples to this value if set."
+            ),
+        },
+    )
+    train_file: Optional[str] = field(
+        default=None,
+        metadata={"help": "A csv or a json file containing the training data."},
+    )
+    validation_file: Optional[str] = field(
+        default=None,
+        metadata={"help": "A csv or a json file containing the validation data."},
+    )
+    test_file: Optional[str] = field(
+        default=None,
+        metadata={"help": "A csv or a json file containing the test data."},
+    )
+    validation_ratio: Optional[float] = field(
+        default=None,
+        metadata={"help": "Percentage of the training data to be used as validation."},
+    )
+    eval_on_test: bool = field(
+        default=False,
+        metadata={"help": "Evaluate the test dataset."},
+    )
+    input_column_names: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": (
+                "name of column to read model input data from. May also be comma "
+                "separated list of two columns to use as inputs. Examples include "
+                "'sentence' for single column and 'sentence_1,sentence_2' for two. "
+                "Default behavior is to read columns based on task name or infer from "
+                "non 'label' columns if sentence_column_names and task name not"
+                "provided"
+            )
+        },
+    )
+    label_column_name: str = field(
+        default="label",
+        metadata={
+            "help": (
+                "column in dataset where input labels are located. Default is 'label'"
+            )
+        },
+    )
+    one_shot: bool = field(
+        default=False,
+        metadata={"help": "Whether to apply recipe in a one shot manner."},
+    )
+    num_export_samples: int = field(
+        default=0,
+        metadata={"help": "Number of samples (inputs/outputs) to export during eval."},
+    )
+
+    def __post_init__(self):
+        if self.task_name is not None:
+            self.task_name = self.task_name.lower()
+            if self.task_name not in _TASK_TO_KEYS.keys():
+                raise ValueError(
+                    "Unknown task, you should pick one in "
+                    + ",".join(_TASK_TO_KEYS.keys())
+                )
+        elif self.dataset_name is not None:
+            pass
+        elif self.train_file is None or self.validation_file is None:
+            raise ValueError(
+                "Need either a GLUE task, a training/validation file or a dataset name"
+            )
+        else:
+            train_extension = self.train_file.split(".")[-1]
+            assert train_extension in [
+                "csv",
+                "json",
+            ], "`train_file` should be a csv or a json file."
+            validation_extension = self.validation_file.split(".")[-1]
+            assert validation_extension == train_extension, (
+                "`validation_file` should have the same extension (csv or json) "
+                "as `train_file`."
+            )
+
+
